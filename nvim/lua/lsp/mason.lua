37888c90fb0f3e488cdc53087ed8f62950b4a092
salo <salo@Veronica.local>
2024-12-23 10:41:22 +0800

    fix: vue lsp

--- nvim/lua/lsp/mason.lua
+++ nvim/lua/lsp/mason.lua
@@ -169,7 +169,7 @@ require('lspconfig').ts_ls.setup({
       {
         name = "@vue/typescript-plugin",
         location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-        languages = {"javascript", "typescript", "vue"},
+        languages = {"javascript", "typescript"},
       },
     },
   },
