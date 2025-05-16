5d5a18cb5f93bc1655884290af047392e490adf6
salo <salo@Veronica.local>
2025-05-16 14:37:31 +0800

    feat: avante confg

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -7,7 +7,8 @@ return {
     -- for example
     provider = "openai",
     openai = {
-      endpoint = "https://api.openai.com/v1",
+      -- endpoint = "https://api.openai.com/v1",
+      endpoint = "https://api.96open.com/v1",
       model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
       temperature = 0,
